a9efc4b6e585909831e0d45fbca09d7061b40136
Muhammad Hanzalah <muhammadhanzalah20000@gmail.com>
2026-01-15 04:33:51 -0800

    fixed the issue agai

--- backend/index.js
+++ backend/index.js
@@ -755,25 +755,9 @@ const fetchUser = async (req, res, next) => {
 };
 
 
-app.post('/addtocart', fetchUser, async (req, res) => {
-    let userData = await Users.findOne({ _id: req.user.id });
-    userData.cartData[req.body.itemId] += 1;
-    await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
-    res.send("Added");
-});
 
-app.post('/removefromcart', fetchUser, async (req, res) => {
-    let userData = await Users.findOne({ _id: req.user.id });
-    if (userData.cartData[req.body.itemId] > 0)
-        userData.cartData[req.body.itemId] -= 1;
-    await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
-    res.send("Removed");
-});
 
-app.post('/getcart', fetchUser, async (req, res) => {
-    let userData = await Users.findOne({ _id: req.user.id });
-    res.json(userData.cartData);
-});
+
 
 // --- API Routes ---
 app.post('/addproduct', async (req, res) => {
@@ -909,6 +893,21 @@ app.post('/reset-password', async (req, res) => {
 app.post('/getcart', fetchUser, async (req, res) => {
     let userData = await Users.findOne({_id: req.user.id});
     res.json(userData.cartData);
+});
+
+app.post('/addtocart', fetchUser, async (req, res) => {
+    let userData = await Users.findOne({ _id: req.user.id });
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
+    res.send("Added");
+});
+
+app.post('/removefromcart', fetchUser, async (req, res) => {
+    let userData = await Users.findOne({ _id: req.user.id });
+    if (userData.cartData[req.body.itemId] > 0)
+        userData.cartData[req.body.itemId] -= 1;
+    await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
+    res.send("Removed");
 });
 
 app.post('/payment', fetchUser, async (req, res) => {
